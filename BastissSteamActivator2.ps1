94648d6fb5431bc7260df9be566144432eb999be
bastisayes <bastisssayes@gmail.com>
2026-09-12 23:13:09 -0300

    v1.6

--- BastissSteamActivator2.ps1
+++ BastissSteamActivator2.ps1
@@ -1,7 +1,7 @@
 $APP_DIR = Join-Path $env:LOCALAPPDATA 'BastissSteam'
 $EXE_PATH = Join-Path $APP_DIR 'BastissSteamActivator2.exe'
 $URL_EXE = 'https://github.com/bastisayes/Fixes-steam/releases/download/bastisss/BastissSteamActivator2.exe'
-$EXPECTED_HASH = '4609FFDD2F84492367EDC7C409B2D4AF9A23C879DE9F121EB271500481004A9F'
+$EXPECTED_HASH = '0669827100CD9714961D27DA67A0862DA22887FD8C6B7F8252CF655B72E10AFC'
 function New-BsaShortcut {
     try {
         $shell = New-Object -ComObject WScript.Shell
